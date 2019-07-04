e463f5cd92e1cf86939eb20d945a53881a6dfad0
Bruno Lima <45238081+brect@users.noreply.github.com>
2019-07-04 00:10:47 -0300

    Criação contador -
    Função criada para incrementar(Up) e decrementar(Down) um valor dentro do input.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+// var btnDown = document.getElementsByClassName('counter-down');
+// var btnUp =   document.getElementsByClassName('counter-up');
+var $contador = $(".controler");    
+$contador.val(0);
+
+$(".counter").click(function(){
+    if($(this).hasClass('counter-up') && ($contador.val()<=9))
+        $contador.val(parseInt($contador.val())+1);
+    if($(this).hasClass('counter-down') && ($contador.val()>=1)) 
+        $contador.val(parseInt($contador.val())-1);
+});
